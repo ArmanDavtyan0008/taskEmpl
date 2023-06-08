f3760a0c91ae4ceaeeff1915f63ec28bc13cdf30
-dav <davtyan.arman1996@gmail.com>
2023-06-08 18:59:51 +0400

    asd

--- src/components/tasks/stylesHome.js
+++ src/components/tasks/stylesHome.js
@@ -1,18 +1,42 @@
+
+
 export const styles = {
   homeMainBox: {
     display: "flex",
     flexDirection: "column",
-    width: { sm: 300, md: 900, lg: 1200, xl: 1500 },
-    mt: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    marginTop: 10,
   },
   buttonOne: {
-    width: { sm: 50, md: 150, lg: 200, xl: 250 },
-    height: { sm: 30, md: 40, lg: 50, xl: 50 },
-    mr: 2,
+    width: {
+      sm: '50px',
+      md: '150px',
+      lg: '200px',
+      xl: '250px'
+    },
+    height: {
+      sm: '30px',
+      md: '40px',
+      lg: '50px',
+      xl: '50px'
+    },
+    marginRight: '2px',
   },
   buttonTwo: {
-    width: { sm: 50, md: 150, lg: 200, xl: 250 },
-    height: { sm: 30, md: 40, lg: 50, xl: 50 },
-    ml: 2,
+    width: {
+      sm: '50px',
+      md: '150px',
+      lg: '200px',
+      xl: '250px'
+    },
+    height: {
+      sm: '30px',
+      md: '40px',
+      lg: '50px',
+      xl: '50px'
+    },
+    marginLeft: '2px',
   },
 };
