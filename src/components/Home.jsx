225ae2b53f739b79cb4d5b271555f14062d906d7
ArmanDavtyan0008 <118016474+ArmanDavtyan0008@users.noreply.github.com>
2023-06-10 11:24:08 +0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,53 @@
-import React, { useEffect } from 'react'
-import { Box, Button } from '@mui/material'
-import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { changePageName } from './redux/slicers/pageSlice'
-import { Description } from './common/Description'
-import { styles } from './tasks/stylesHome'
+import React, { useEffect } from "react";
+import { Box, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { changePageName } from "./redux/slicers/pageSlice";
+import { Description } from "./common/Description";
+import { styles } from "./tasks/stylesHome";
 
 export const Home = () => {
-  const dispatch = useDispatch()
-  const page = useSelector(state => state.page)
-  useEffect(() => {
-    dispatch(changePageName('home'))
-  }, [page])
-  
+  const dispatch = useDispatch();
+  const page = useSelector((state) => state.page);
+
+
+  const onHandleCLickEmpl = ()=>{
+    dispatch(changePageName("home"))
+  }
+
   return (
-    <Box sx={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}> 
-    <Box sx={{...styles.homeMainBox}}>  
-    <Description />
-    <Box sx= {{display:'flex', justifyContent:'center', alignItems:'center', width:{sm:300,md:900,lg:1200, xl:1500},  mt:6 }} > 
-    <Link to={page.currentPage ==='home'? '/employees': '/'}>
-    <Button variant='contained'sx={{...styles.buttonOne}}> Employees</Button>
-  </Link>
-  <Link to={page.secondPage === 'tasks'? '/tasks': '/employees'}>
-      <Button variant='contained' sx={{...styles.buttonTwo}}>
-       {page.secondPage === 'tasks' ? "Tasks" : "Employees"}</Button>
-    </Link>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box sx={{ ...styles.homeMainBox }}>
+        <Description />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: { sm: 300, md: 900, lg: 1200, xl: 1500 },
+            mt: 6,
+          }}
+        >
+          <Link to={page.currentPage === "home" ? "/employees" : "/"}>
+            <Button variant="contained" sx={{ ...styles.buttonOne }} onClick={onHandleCLickEmpl}>
+              {" "}
+              Employees
+            </Button>
+          </Link>
+          <Link to={page.secondPage === "tasks" ? "/tasks" : "/employees"}>
+            <Button variant="contained" sx={{ ...styles.buttonTwo }}>
+              {page.secondPage === "tasks" ? "Tasks" : "Employees"}
+            </Button>
+          </Link>
+        </Box>
+      </Box>
     </Box>
-    </Box>
-    </Box>
-  )
-}
+  );
+};
